39706e36b1227acef8bc0afe700ea159c9e0a8e2
SaiMowli <dasaimouli@gmail.com>
2022-04-10 13:49:51 +0530

    Column 1 completed

--- numpyscript.py
+++ numpyscript.py
@@ -61,8 +61,56 @@ def cs_body():
      # 5x5 array of 0 with 1 on diagonal(identity matrix)
      np.linespace(0,100,6)
      # Arrray of 6 evenly divided values from 0 to 100
+     np.arrange(0,10,3)
+     # Array of values from 0 to less than 10 with step 3
+     #Eg:- [0,3,6,9]
+     np.full((2,3),8)
+     # 2x3 array  with all values 8
+     np.random.rand(4,5)
+     # 4x5 Array of random floats between 0-1
+     np.random.rand(6,7)*100
+     # 6x7 Array of random floats between 0-100
+     np.random.randint(5,size=(2,3))
+     # 2x3 array with random ints between 0-4
      ''')
 
+    col1.subheader('Inspecting Properties')
+    col1.code('''
+     arr.size
+     # Returns number of elements in arr
+     arr.shape
+     # Returns dimensions of arr(Rows,Columns)
+     arr.dtype 
+     # Returns type f elements in  the arr
+     arr.astype(dtype)
+     # Convert arr element to type dtype
+     arr.tolist()
+     # Convert arr to a python list
+     np.info(np.eye)
+     # View documentation for np.eye
+     ''')
+
+    col1.subheader('Copying/Sorting/Reshaping')
+    col1.code('''
+    np.copy(arr)
+    # Copies arr to new memory
+    arr.view(dtype)
+    # Creates view of arr elements with type dtype
+    arr.sort()
+    # Sort arr
+    arr.sort(axis=0)
+    # Sorts specific axis of arr
+    two_d_arr.flatten()
+    # Flatten 2D array two_d_arr to 1D
+    arr.T
+    # Transposes arr(Rows become Columns and Vise Versa)
+    arr.reshape(3,4)
+    # Reshapes arr to 3 roe=ws and 4 columns without changing the data
+    arr.resize((5,6))
+    #Changes arr shape to 5x6 and fills new values with 0
+    ''')
+
+    
 
 
 
@@ -73,6 +121,5 @@ def main():
     # cs_sidebar()
     cs_body()
 
-    st.header("XYZZ")
 
     return None
