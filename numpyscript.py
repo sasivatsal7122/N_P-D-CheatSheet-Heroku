83c9c3042c2ad21b7dd434af15a4d32871318212
SaiMowli <dasaimouli@gmail.com>
2022-04-10 15:00:27 +0530

    2nd column chesi 10ga annawww

--- numpyscript.py
+++ numpyscript.py
@@ -101,7 +101,8 @@ def cs_body():
     arr.sort(axis=0)
     # Sorts specific axis of arr
     two_d_arr.flatten()
-    # Flatten 2D array two_d_arr to 1D
+    # Flatten 2D array two_d_arr to 1D''')
+    col2.code('''
     arr.T
     # Transposes arr(Rows become Columns and Vise Versa)
     arr.reshape(3,4)
@@ -138,7 +139,38 @@ def cs_body():
     # Split arr horizontally on the 5th index
     ''')
 
-    #
+    # Indexing/Slising/Subsetting
+
+    col2.subheader('Indexing/Slising/Subsetting')
+    col2.code('''
+    arr[5]
+    # Returns the element at index 5
+    arr[2,5]
+    # Return the 2D array element on the index [2][5]
+    arr[1]=4
+    # Assign array element on index 1 to the value 4
+    arr[1,3]=10
+    # Assign array element on index [1][3] to the value 10
+    arr[0:3]
+    # Returns the element at the indicces 0,1,2(On the 2D array: returns 0,1,2)
+    arr[0:3,4]
+    # returns the elements on the rows 0,1,2 at column 4
+    arr[:2]
+    # Returns the elements at the indices 0,1(On a 2D array:returns rows 0,1)
+    arr[:,1]
+    # Returns the elements at the index 1 on all rows
+    arr<5
+    # Returns an array of boolean values
+    (arr1<3)&(arr2<5)
+    # Returns an array of boolean values
+    ~arr
+    # Inverts a boolean array
+    arr[arr<5]
+    # Returns array elements smaller than 5
+    ''')
+
+    # Scalar Math
+    
 
 
 
