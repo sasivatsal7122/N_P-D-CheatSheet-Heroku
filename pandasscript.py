67b802d10e9632e5faf24cf7a57b2dea2f06f20a
SasiVatsal <71326149+sasivatsal7122@users.noreply.github.com>
2022-04-14 14:10:46 +0530

    asdasd

--- pandasscript.py
+++ pandasscript.py
@@ -19,30 +19,30 @@ def cs_body():
         index=[1,2,3],
         columns=['a','b','c'])
     col1, col2, col3 = st.columns(3)
-    col11,col12=st.columns(2)
     st.text('')
     col1.subheader('Creating DataFrames')
     col1.code('''
-        # Spesify values for each column
-        df=pd.DataFrame({
-        "a" : [4,5,6],
-        "b": [7,8,9],
-        "c": [10,11,12]},
-        index=[1,2,3])
+# Spesify values for each column
+df=pd.DataFrame({
+"a" : [4,5,6],
+"b": [7,8,9],
+"c": [10,11,12]},
+index=[1,2,3])
 
-        # Specify values for each row
-        df1=pd.DataFrame([
-         [4,7,10],
-         [5,8,11],
-         [6,9,12]],
-        index=[1,2,3],
-        columns=['a','b','c'])
-        
+# Specify values for each row
+df1=pd.DataFrame([
+    [4,7,10],
+    [5,8,11],
+    [6,9,12]],
+index=[1,2,3],
+columns=['a','b','c'])
+
 
     ''')
-    st.text('Output')
-    col1.col11.st.dataframe(df)
-    col1.col12.st.dataframe(df1)
+    col1.text('Output : ')
+    
+    col1.dataframe(df)
+    col1.dataframe(df1)
     
 
 
