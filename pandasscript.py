1ce0459d0c9025c608c98dd343226cac9aca0f5c
SaiMowli <dasaimouli@gmail.com>
2022-04-29 18:49:02 +0530

    commit

--- pandasscript.py
+++ pandasscript.py
@@ -12,15 +12,29 @@ def cs_body():
         "b": [7,8,9],
         "c": [10,11,12]},
         index=[1,2,3])
-    df1=pd.DataFrame([
-         [4,7,10],
-         [5,8,11],
-         [6,9,12]],
-        index=[1,2,3],
-        columns=['a','b','c'])
+    df1=pd.DataFrame({
+        "a" : [4,5,6],
+        "b": [7,8,9],
+        "c": [10,11,12]},
+        index=pd.MultiIndex.from_tuples(
+            [('d',1),('d',2),('e',2)],names=['n','v']
+            ))
     col1, col2, col3 = st.columns(3)
     st.text('')
     col1.subheader('Creating DataFrames')
+    col1.code('''
+# Spesify values for each column
+>>> df1=pd.DataFrame({
+        "a" : [4,5,6],
+        "b": [7,8,9],
+        "c": [10,11,12]},
+        index=pd.MultiIndex.from_tuples(
+            [('d',1),('d',2),('e',2)],names=['n','v']
+            ))
+
+
+    ''')
+
     col1.code('''
 # Spesify values for each column
 >>> df=pd.DataFrame({
@@ -40,6 +54,7 @@ columns=['a','b','c'])
 
     ''')
     col1.text('Output : ')
+    
     
     col1.dataframe(df)
     col1.dataframe(df1)
