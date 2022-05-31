b31b549242f1a21b6b8f755ae409e92a30e1ca01
SaiMowli <dasaimouli@gmail.com>
2022-05-31 09:56:11 +0530

    Added

--- pandasscript.py
+++ pandasscript.py
@@ -114,8 +114,45 @@ columns=['a','b','c'])
  df.drop(columns=['Length', 'Height'])
  #Drop columns from DataFrame
     ''')
-
     
+    col2.subheader('Subset Observations')
+    col2.text('-Rows')
+    col2.code('''
+ df[df.Length > 7]
+ #Extract rows that meet logical criteria.
+ df.drop_duplicates()
+ #Remove duplicate rows (only considers columns).
+ df.sample(frac=0.5)
+ #Randomly select fraction of rows.
+ df.sample(n=10)
+ #Randomly select n rows.
+ df.nlargest(n, 'value')
+ #Select and order top n entries.
+ df.nsmallest(n, 'value')
+ #Select and order bottom n entries.
+ df.head(n)
+ #Select first n rows.
+ df.tail(n)
+ #Select last n rows.
+    ''')
+    col2.subheader('Subset Variables')
+    col2.text('-Columns')
+    col2.code('''
+ df[['width', 'length', 'species']]
+ #Select multiple columns with specific names.
+ df['width'] or df.width
+ #Select single column with specific name.
+ df.filter(regex='regex')
+ #Select columns whose name matches regular expression regex.
+    ''')
+    col2.subheader('Using Query')
+    col2.code('''
+ #query() allows Boolean 
+ #expressions for filtering rows.
+ df.query('Length > 7')
+ df.query('Length > 7 and Width < 8')
+ df.query('Name.str.startswith("abc")',engine="python")
+    ''')
 
 
 
