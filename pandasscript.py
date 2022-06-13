499999417a70817fae2a56e3748cd2d1c107699d
SasiVatsal <71326149+sasivatsal7122@users.noreply.github.com>
2022-06-13 19:39:45 +0530

    deleted readme

--- pandasscript.py
+++ pandasscript.py
@@ -71,7 +71,7 @@ columns=['a','b','c'])
  #Most pandas methods return a DataFrame so that
  #another pandas method can be applied to the result.
  #This improves readability of code.
- df = (pd.melt(df)
+ >>> df = (pd.melt(df)
  .rename(columns={
  'variable':'var',
  'value':'val'})
@@ -85,30 +85,30 @@ columns=['a','b','c'])
     col1.code('''
  #query() allows Boolean 
  #expressions for filtering rows.
- df.query('Length > 7')
- df.query('Length > 7 and Width < 8')
- df.query('Name.str.startswith("abc")',engine="python")
+ >>> df.query('Length > 7')
+ >>> df.query('Length > 7 and Width < 8')
+ >>> df.query('Name.str.startswith("abc")',engine="python")
     ''')
 
     col1.subheader('Subset Variables')
     col1.text('-Columns')
     col1.code('''
- df[['width', 'length', 'species']]
+ >>> df[['width', 'length', 'species']]
  #Select multiple columns with specific names.
- df['width'] or df.width
+ >>> df['width'] or df.width
  #Select single column with specific name.
- df.filter(regex='regex')
+ >>> df.filter(regex='regex')
  #Select columns whose name matches regular expression regex.
     ''')
 
     col1.subheader('Windows')
     col1.code('''
-    df.expanding()
-    #Return an Expanding object allowing summary functions to be
-    #applied cumulatively.
-    df.rolling(n)
-    #Return a Rolling object allowing summary functions to be
-    #applied to windows of length n.
+>>> df.expanding()
+#Return an Expanding object allowing summary functions to be
+#applied cumulatively.
+>>> df.rolling(n)
+#Return a Rolling object allowing summary functions to be
+#applied to windows of length n.
     ''')
 
     col2.subheader('Reshaping Data')
@@ -122,47 +122,47 @@ columns=['a','b','c'])
     
 
     col2.code('''
- pd.melt(df)
+ >>> pd.melt(df)
  #Gather columns into rows.
- df.pivot(columns='var', values='val')
+ >>> df.pivot(columns='var', values='val')
  # Spread rows into columns.
- pd.concat([df1,df2])
+ >>> pd.concat([df1,df2])
  #Append rows of DataFrames
- pd.concat([df1,df2], axis=1)
+ >>> pd.concat([df1,df2], axis=1)
  #Append columns of DataFrames
- df.sort_values('mpg')
+ >>> df.sort_values('mpg')
  #Order rows by values of a column (low to high).
- df.sort_values('mpg', ascending=False)
+ >>> df.sort_values('mpg', ascending=False)
  #Order rows by values of a column (high to low).
- df.rename(columns = {'y':'year'})
+ >>> df.rename(columns = {'y':'year'})
  #Rename the columns of a DataFrame
- df.sort_index()
+ >>> df.sort_index()
  #Sort the index of a DataFrame
- df.reset_index()
+ >>> df.reset_index()
  #Reset index of DataFrame to row numbers, moving
  #index to columns.
- df.drop(columns=['Length', 'Height'])
+ >>> df.drop(columns=['Length', 'Height'])
  #Drop columns from DataFrame
     ''')
     
     col2.subheader('Subset Observations')
     col2.text('-Rows')
     col2.code('''
- df[df.Length > 7]
+ >>> df[df.Length > 7]
  #Extract rows that meet logical criteria.
- df.drop_duplicates()
+ >>> df.drop_duplicates()
  #Remove duplicate rows (only considers columns).
- df.sample(frac=0.5)
+ >>> df.sample(frac=0.5)
  #Randomly select fraction of rows.
- df.sample(n=10)
+ >>> df.sample(n=10)
  #Randomly select n rows.
- df.nlargest(n, 'value')
+ >>> df.nlargest(n, 'value')
  #Select and order top n entries.
- df.nsmallest(n, 'value')
+ >>> df.nsmallest(n, 'value')
  #Select and order bottom n entries.
- df.head(n)
+ >>> df.head(n)
  #Select first n rows.
- df.tail(n)
+ >>> df.tail(n)
  #Select last n rows.
     ''')
 
@@ -175,120 +175,120 @@ columns=['a','b','c'])
  #Use df.at[] and df.iat[] to access a single
  #value by row and column.
  #First index selects rows, second index columns.
- df.iloc[10:20]
+ >>> df.iloc[10:20]
  #Select rows 10-20.
- df.iloc[:, [1, 2, 5]]
+ >>> df.iloc[:, [1, 2, 5]]
  #Select columns in positions 1, 2 and 5 (first column is 0).
- df.loc[:, 'x2':'x4']
+ >>> df.loc[:, 'x2':'x4']
  #Select all columns between x2 and x4 (inclusive).
- df.loc[df['a'] > 10, ['a', 'c']]
+ >>> df.loc[df['a'] > 10, ['a', 'c']]
  #Select rows meeting logical condition, and only the specific columns.
- df.iat[1,2] #Access single value by index
- df.at[4, 'A'] #Access single value by label
+ >>> df.iat[1,2] #Access single value by index
+ >>> df.at[4, 'A'] #Access single value by label
     ''')
 
     col2.subheader('Handling Missing Data')
     col2.code('''
- df.dropna()
+ >>> df.dropna()
  #Drop rows with any column having NA/null data.
- df.fillna(value)
+ >>> df.fillna(value)
  #Replace all NA/null data with value.
     ''')
     col2.subheader('Plotting')
     col2.code('''
- df.plot.hist()
+ >>> df.plot.hist()
  #Histogram for each column
- df.plot.scatter(x='w',y='h')
+ >>> df.plot.scatter(x='w',y='h')
  #Scatter chart using pairs of points.
     ''')
     
     col3.subheader('Summarize Data')
     col3.code('''
- df['w'].value_counts()
+ >>> df['w'].value_counts()
  #Count number of rows with each unique value of variable
- len(df)
+ >>> len(df)
  #Number of rows in DataFrame.
- df.shape
+ >>> df.shape
  #Tuple of no.of rows, no.of columns in DataFrame.
- df['w'].nunique()
+ >>> df['w'].nunique()
  #No.of distinct values in a column.
- df.describe()
+ >>> df.describe()
  #Basic descriptive and statistics for each column (or GroupBy).
     ''')
     col3.subheader('Summary Functions')
     col3.code('''
-    #Pandas provides a large set of summary functions that operate on
-    #different kinds of pandas objects (DataFrame columns, Series,
-    #GroupBy, Expanding and Rolling (see below)) and produce single
-    #values for each of the groups. When applied to a DataFrame, the
-    #result is returned as a pandas Series for each column.
-    sum() - #Sum values of each object. 
-    min() - #Minimum value in each object.
-    max() - #Maximum value in each object.
-    count() - #Count non-NA/null values of each object.
-    mean() - #Mean value of each object.
-    median() - #Median value of each object.
-    var() - #Variance of each object.
-    quantile([0.25,0.75]) - #Quantiles of each object.
-    std() - #Standard deviation of each object.
-    apply(function) - #Apply function to each object.
+#Pandas provides a large set of summary functions that operate on
+#different kinds of pandas objects (DataFrame columns, Series,
+#GroupBy, Expanding and Rolling (see below)) and produce single
+#values for each of the groups. When applied to a DataFrame, the
+#result is returned as a pandas Series for each column.
+sum() - #Sum values of each object. 
+min() - #Minimum value in each object.
+max() - #Maximum value in each object.
+count() - #Count non-NA/null values of each object.
+mean() - #Mean value of each object.
+median() - #Median value of each object.
+var() - #Variance of each object.
+quantile([0.25,0.75]) - #Quantiles of each object.
+std() - #Standard deviation of each object.
+apply(function) - #Apply function to each object.
     ''')    
     col3.subheader('Make New Columns')
     col3.code('''
-    df.assign(Area=lambda df: df.Length*df.Height)
-    #Compute and append one or more new columns.
-    df['Volume'] = df.Length*df.Height*df.Depth
-    #Add single column.
-    pd.qcut(df.col, n, labels=False)
-    #Bin column into n buckets.
+>>> df.assign(Area=lambda df: df.Length*df.Height)
+#Compute and append one or more new columns.
+>>> df['Volume'] = df.Length*df.Height*df.Depth
+#Add single column.
+>>> pd.qcut(df.col, n, labels=False)
+#Bin column into n buckets.
 
-    #Pandas provides a large set of vector functions that operate on all
-    #columns of a DataFrame or a single selected column (a pandas
-    #Series). These functions produce vectors of values for each of the
-    #columns, or a single Series for the individual Series. Examples:
-    max(axis=1)
-    #Element-wise max.
-    min(axis=1)
-    #Element-wise min.
-    clip(lower=-10,upper=10)
-    #Trim values at input thresholds
-    abs()
+#Pandas provides a large set of vector functions that operate on all
+#columns of a DataFrame or a single selected column (a pandas
+#Series). These functions produce vectors of values for each of the
+#columns, or a single Series for the individual Series. Examples:
+max(axis=1)
+#Element-wise max.
+min(axis=1)
+#Element-wise min.
+clip(lower=-10,upper=10)
+#Trim values at input thresholds
+abs()
     #Absolute value.
     ''')
 
     col3.subheader('Group Data')
     col3.code('''
-    df.groupby(by="col")
-    #Return a GroupBy object, grouped
-    #by values in column named "col".
-    df.groupby(level="ind")
-    #Return a GroupBy object, grouped
-    #by values in index level named
-    #"ind".
+>>> df.groupby(by="col")
+#Return a GroupBy object, grouped
+#by values in column named "col".
+>>> df.groupby(level="ind")
+#Return a GroupBy object, grouped
+#by values in index level named
+#"ind".
 
-    #The examples below can also be applied to groups. In this case, the
-    #function is applied on a per-group basis, and the returned vectors
-    #are of the length of the original DataFrame.
-    shift(1)
-    #Copy with values shifted by 1.
-    shift(-1)
-    #Copy with values lagged by 1.
-    rank(method='dense')
-    #Ranks with no gaps.
-    rank(method='min')
-    #Ranks. Ties get min rank.
-    rank(pct=True)
-    #Ranks rescaled to interval [0, 1].
-    rank(method='first')
-    #Ranks. Ties go to first value.
-    cumsum()
-    #Cumulative sum.
-    cummax()
-    #Cumulative max.
-    cummin()
-    #Cumulative min.
-    cumprod()
-    #Cumulative product.
+#The examples below can also be applied to groups. In this case, the
+#function is applied on a per-group basis, and the returned vectors
+#are of the length of the original DataFrame.
+shift(1)
+#Copy with values shifted by 1.
+shift(-1)
+#Copy with values lagged by 1.
+rank(method='dense')
+#Ranks with no gaps.
+rank(method='min')
+#Ranks. Ties get min rank.
+rank(pct=True)
+#Ranks rescaled to interval [0, 1].
+rank(method='first')
+#Ranks. Ties go to first value.
+cumsum()
+#Cumulative sum.
+cummax()
+#Cumulative max.
+cummin()
+#Cumulative min.
+cumprod()
+#Cumulative product.
     ''')
 
 
